731bea5eb86313e571895154e03848b7d595ab7c
Linhe Xu <linhe.xu@hotmail.com>
2020-05-12 15:47:03 -0500

    included all mouse protein coding genes dNdS scores and mouse organ specific genes

---
+++ bash_scripts/mouse_organs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for file in ../data/MGI_organs/*-specific.txt
+do
+	tissue=$(echo ${file} | cut -c 20- | rev | cut -c 14- | rev)
+	#echo ${tissue}
+	awk -v var=${tissue} 'NR>1{print $1,var}' OFS='\t' ${file} >> ../results/MGI_organs/MGI_ID-tissue_type.tsv
+done
+awk '{print $1}' ../results/MGI_organs/MGI_ID-tissue_type.tsv > ../results/MGI_organs/MGI_ID_list.txt
