901709e1607cec61574a1aef77366b5f4e30a3c5
Linhe Xu <linhe.xu@hotmail.com>
2022-12-08 20:35:36 -0600

    data file and old sh files

---
+++ bash_scripts/all_chicken_protein_coding_dNdS.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while read SPECIES
+do
+	wget -O ../results/Ensembl98_chicken/chicken_protein_coding_genes.${SPECIES}.txt "http://www.ensembl.org/biomart/martservice?query=<?xml version=\"1.0\" encoding=\"UTF-8\"?><Query  virtualSchemaName = \"default\" formatter = \"TSV\" header = \"0\" uniqueRows = \"0\" count = \"\" datasetConfigVersion = \"0.6\" ><Dataset name = \"ggallus_gene_ensembl\" interface = \"default\" ><Filter name = \"biotype\" value = \"protein_coding\"/><Attribute name = \"ensembl_gene_id\" /><Attribute name = \"external_gene_name\" /><Attribute name = \"description\" /><Attribute name = \"${SPECIES}_homolog_orthology_type\" /><Attribute name = \"${SPECIES}_homolog_subtype\" /><Attribute name = \"${SPECIES}_homolog_dn\" /><Attribute name = \"${SPECIES}_homolog_ds\" /><Attribute name = \"${SPECIES}_homolog_orthology_confidence\" /></Dataset></Query>"
+done < ../bash_scripts/all_chicken_protein_coding_dNdS.sh
